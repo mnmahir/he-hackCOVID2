0aff270391d5260e4863b36ac0f0c4b8e98bab1c
aslamkhan1998 <67847981+aslamkhan1998@users.noreply.github.com>
2021-06-25 15:31:10 +0800

    Enabled camera feed
    Enabled the camera feed. The laptop webcam will be turned on and the video feed will be mirrored so that it would look and feel normal

--- The best covid system/Detection System/main_test.py
+++ The best covid system/Detection System/main_test.py
@@ -1,6 +1,30 @@
 import maskDetect
 import socDistDetect
 import video
+import argparse
+import time
+import sys
+import cv2
+import numpy
+import imutils
 
-print("Hello World!!")
-print("Aslam")
+cap = cv2.VideoCapture(0)
+
+#Check if webcame is available or not
+if not cap.isOpened():
+    raise IOError("Webcam not available")
+
+while True:
+    ret, frame = cap.read()
+    frame = cv2.resize(frame, None, fx=1.5, fy=1.5, interpolation = cv2.INTER_CUBIC)
+    #Mirrors the input camera footage
+    frame_flip = cv2.flip(frame,1)
+    cv2.imshow('Camera', frame_flip)
+    
+    #Press 'q' to close the camera feed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+    
+cap.release()
+cv2.destroyAllWindows()
